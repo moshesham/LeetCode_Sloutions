80bbda7495f62831d6d1bca598faada4da5bbc67
Moshe Shamouilian <Moshesham@gmail.com>
2024-11-20 19:21:19 +0200

    Create file

---
+++ 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) < 3:
+            return False
+        
+        first = float('inf')
+        second = float('inf')
+        
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        
+        return False
+
