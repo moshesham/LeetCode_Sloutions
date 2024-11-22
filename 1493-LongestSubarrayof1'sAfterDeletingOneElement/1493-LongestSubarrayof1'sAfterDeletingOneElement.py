7b700ad77a470aeb5be14729f7c6cd6eb65a091f
Moshe Shamouilian <Moshesham@gmail.com>
2024-11-22 03:04:48 +0200

    Create file

---
+++ 1493-LongestSubarrayof1'sAfterDeletingOneElement/1493-LongestSubarrayof1'sAfterDeletingOneElement.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        max_length = 0
+        zero_count = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+
+            while zero_count > 1:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+
+            max_length = max(max_length, right - left + 1)
+
+        # Since we need to delete one element, the result is max_length - 1
+        return max_length - 1
