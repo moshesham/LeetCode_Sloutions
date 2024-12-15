e9109a63e1e299123365424bf40453e737513390
Moshe Shamouilian <Moshesham@gmail.com>
2024-12-15 20:57:31 +0200

    Update file 12/15/2024, 8:57:31 PM

--- 1667-FixNamesinaTable/1667-FixNamesinaTable.py
+++ 1667-FixNamesinaTable/1667-FixNamesinaTable.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-def fix_names(users: pd.DataFrame) -> pd.DataFrame:
-    users["name"] = users["name"].str.capitalize()
-    return users.sort_values('user_id')
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    email_regex = r"^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\.com$"
+    valid_emails = users[users['mail'].str.match(email_regex)]
+
+    return valid_emails
+
+
+
+    
